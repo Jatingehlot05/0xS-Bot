4b9001f4d7cbbb68f38aaa7096c2c12296b22056
Jatingehlot05 <79295463+Jatingehlot05@users.noreply.github.com>
2026-01-02 15:05:56 +0530

    Update config.js to use environment variables

--- config.js
+++ config.js
@@ -2,14 +2,14 @@ require('dotenv').config();
 
 module.exports = {
   SESSION_ID: process.env.SESSION_ID || '',
+  PHONE_NUMBER: process.env.PHONE_NUMBER || '',
   PREFIX: process.env.PREFIX || '.',
   BOT_NAME: process.env.BOT_NAME || '0xS Bot',
-  OWNER_NUMBER: process.env.OWNER_NUMBER || '919664413727',
-  OWNER_NAME: process.env.OWNER_NAME || 'IAMJ',
+  OWNER_NUMBER: process.env.OWNER_NUMBER || '',
+  OWNER_NAME: process.env.OWNER_NAME || 'Jatin Gehlot',
   MODE: process.env.MODE || 'public',
-  WELCOME: process.env.WELCOME === 'true',
+  WELCOME: process.env.WELCOME === 'false',
   GOODBYE: process.env.GOODBYE === 'false',
-  AUTO_READ: process.env.AUTO_READ === 'false',
-  AUTO_STATUS_VIEW: process.env.AUTO_STATUS_VIEW === 'false',
+  AUTO_READ: process.env.AUTO_READ === 'true',
   SUDO: process.env.SUDO ? process.env.SUDO.split(',').map(n => n.trim()) : []
 };
